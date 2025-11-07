bbbd3f68b1bb23198afc7807db801f69c62d6574
SYRRUS-Ali <152086107+SYRRUS-Ali@users.noreply.github.com>
2025-11-07 19:18:58 +0300

    Add Fake Erorr - 1

--- library_project/library_app/views.py
+++ library_project/library_app/views.py
@@ -26,12 +26,6 @@ from .forms import (
     BookFacultyUsageForm
 )
 
-def badFunction():
-    x = 1
-    y = 2
-    print(X + y)  # خطأ: X غير معرّف
-
-
 # Custom exception handlers
 def handler403(request, exception):
     return render(request, 'library_app/errors/403.html', status=403)
@@ -1208,3 +1202,4 @@ def api_branch_books(request, branch_id):
     
 
     return JsonResponse(data)
+
